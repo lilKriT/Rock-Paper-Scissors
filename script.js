0abcf0ddcc24a3e8f858cfb6ff77032dfa36ca50
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-07 20:42:50 +0200

    Completed.

--- script.js
+++ script.js
@@ -4,10 +4,43 @@ const resultDisplay = document.getElementById("result");
 
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
+let computerChoice;
+let result;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
   })
 );
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length);
+  console.log(randomNumber);
+
+  if (randomNumber === 0) {
+    computerChoice = "rock";
+  } else if (randomNumber === 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "draw";
+  } else if (
+    (computerChoice === "rock" && userChoice === "paper") ||
+    (computerChoice === "paper" && userChoice === "scissors") ||
+    (computerChoice === "scissors" && userChoice === "rock")
+  ) {
+    result = "you win!";
+  } else {
+    result = "you lose :(";
+  }
+  resultDisplay.innerHTML = result;
+}
